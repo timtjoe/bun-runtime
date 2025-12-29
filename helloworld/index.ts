b11e0b38433239780dada7561c6ffdff2a4c9a96
timtjoe <ttjoe0@gmail.com>
2025-12-29 12:13:55 +0000

    complete html

--- helloworld/index.ts
+++ helloworld/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import index from "./index.html";
+
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/": index,
+  },
+});
+
+console.log(`Listening on ${server.url}`);
